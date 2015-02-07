6a227347370a1a6cd4e1c0b3a67d8aa3ae265b10
Ke Ding <kedingpku@gmail.com>
2015-02-08 00:44:11 +0800

    uddate R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
+## wrap a normal matrix so that cache inverve can 
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(.mat = matrix()) {
+    .inv <- NULL
+    set <- function(mat) {
+        .mat <<- mat
+        .inv <<- NULL
+    }
+    get <- function() .mat
+    setinv <- function(inv) .inv <<- inv
+    getinv <- function() .inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
-
 ## Write a short comment describing this function
+cacheSolve <- function(mat, ...) {
+    inv <- mat$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- mat$get()
+    inv <- solve(data, ...)
+    mat$setinv(inv)
+    inv
+}
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Test using Hilbert matrix
+## note that Hilbert matrices are famous for ill condition
+hilbert <- function(n) { 
+    i <- 1:n
+    1 / outer(i - 1, i, "+") 
+}
+x <- hilbert(8);
+y <- makeCacheMatrix(x)
+for (i in 1:10) {
+	i
+    cacheSolve(y)
 }
